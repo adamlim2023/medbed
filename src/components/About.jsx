31eedada1740a98409cc8e7a869194f98c938fe0
adamlim2023 <adamlim2023@gmail.com>
2024-04-22 23:43:34 +0800

    video

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,30 +5,29 @@ import PlayIcon from "assets/images/icons/play.svg";
 
 const About = () => {
   const videoRef = useRef();
-  const [playing, setPlaying] = useState(false);
+  const [opened, setOpened] = useState(false);
 
   const handlePlay = () => {
-    videoRef.current.play();
-    setPlaying(true);
+    handleToggleOpen();
+    setTimeout(() => {
+      videoRef.current.play();
+    }, 100);
   };
 
+  const handleToggleOpen = () => setOpened(!opened);
+
   return (
-    <div className="flex flex-col lg:flex-row items-center gap-7 lg:gap-14">
-      <motion.div
-        initial={{ x: -350, opacity: 0 }}
-        whileInView={{ x: 0, opacity: 1 }}
-        transition={{ duration: 1, ease: "backOut" }}
-        className="w-full max-w-[407px] h-[380px] flex-shrink-0 relative rounded-[29px] lg:rounded-[40px] overflow-hidden"
-      >
-        <video
-          className="w-full h-full object-cover"
-          play={true}
-          ref={videoRef}
-          onEnded={() => setPlaying(false)}
+    <>
+      <div className="flex flex-col lg:flex-row items-center gap-7 lg:gap-14">
+        <motion.div
+          initial={{ x: -350, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1, ease: "backOut" }}
+          className="w-full max-w-[407px] h-[380px] flex-shrink-0 relative rounded-[29px] lg:rounded-[40px] overflow-hidden"
         >
-          <source src={Video} type="video/mp4" />
-        </video>
-        {!playing && (
+          <video className="w-full h-full object-cover">
+            <source src={Video} type="video/mp4" />
+          </video>
           <button
             className="w-[86px] h-[86px] flex items-center justify-center rounded-full border border-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white/10 backdrop-blur-[5px] backdrop-brightness-[1.15] transition hover:opacity-80"
             onClick={handlePlay}
@@ -37,34 +36,50 @@ const About = () => {
               <img src={PlayIcon} />
             </div>
           </button>
-        )}
-      </motion.div>
-      <motion.div
-        initial={{ x: 200, opacity: 0 }}
-        whileInView={{ x: 0, opacity: 1 }}
-        transition={{ duration: 1, ease: "backOut", delay: 0.5 }}
-      >
-        <p className="font-bold text-[25px] lg:text-4xl mb-3.5 lg:mb-6">
-          About Medbed
-        </p>
-        <p className="font-light text-[15px] lg:text-sm mb-9 leading-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doiusmodt
-          emporw cididunt ut labore et dolore magna aliquaenim ad minim veniam,
-          quis nostruex ercitation ullamco laboris nisi ut aliquip ex ea como do
-          consequat loremono ita lianost dolore magna aliqu oris nisi ut aliq
-          cididunt ut labore et dolore magna a liquaenim ad minim veniam, quis
-          nostruex <br />
-          <br />
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doiusmodt
-          emporw cididunt ut labore et dolore magna aliquaenim ad minim veniam,
-          quis nostruex ercitation ullamco laboris nisi ut aliquip ex ea como do
-          consequat loremono ita lianost dolore magna aliqu oris nisi ut aliq
-        </p>
-        <button className="w-full lg:w-auto h-11 px-11 rounded-[11px] font-medium text-sm bg-gradient-to-br from-[#F7DEE8] via-[#E7E6FC] via-30% via-[#E4F1FE] via-70% to-[#E8EEF3] hover:opacity-50 transition">
-          View Details
-        </button>
-      </motion.div>
-    </div>
+        </motion.div>
+        <motion.div
+          initial={{ x: 200, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1, ease: "backOut", delay: 0.5 }}
+        >
+          <p className="font-bold text-[25px] lg:text-4xl mb-3.5 lg:mb-6">
+            About Medbed
+          </p>
+          <p className="font-light text-[15px] lg:text-sm mb-9 leading-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            doiusmodt emporw cididunt ut labore et dolore magna aliquaenim ad
+            minim veniam, quis nostruex ercitation ullamco laboris nisi ut
+            aliquip ex ea como do consequat loremono ita lianost dolore magna
+            aliqu oris nisi ut aliq cididunt ut labore et dolore magna a
+            liquaenim ad minim veniam, quis nostruex <br />
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            doiusmodt emporw cididunt ut labore et dolore magna aliquaenim ad
+            minim veniam, quis nostruex ercitation ullamco laboris nisi ut
+            aliquip ex ea como do consequat loremono ita lianost dolore magna
+            aliqu oris nisi ut aliq
+          </p>
+          <button className="w-full lg:w-auto h-11 px-11 rounded-[11px] font-medium text-sm bg-gradient-to-br from-[#F7DEE8] via-[#E7E6FC] via-30% via-[#E4F1FE] via-70% to-[#E8EEF3] hover:opacity-50 transition">
+            View Details
+          </button>
+        </motion.div>
+      </div>
+      {opened && (
+        <div className="fixed top-0 left-0 w-full h-screen flex items-center justify-center z-50">
+          <div
+            className="absolute top-0 left-0 w-full h-full bg-black opacity-40"
+            onClick={handleToggleOpen}
+          />
+          <video
+            className="w-full max-w-5xl object-cover relative z-10"
+            play={true}
+            ref={videoRef}
+          >
+            <source src={Video} type="video/mp4" />
+          </video>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/components/Containers/Disease/Hero.jsx
+++ src/components/Containers/Disease/Hero.jsx
@@ -4,66 +4,77 @@ import PlayIcon from "assets/images/icons/play.svg";
 
 const Hero = () => {
   const videoRef = useRef();
-  const [playing, setPlaying] = useState(false);
+  const [opened, setOpened] = useState(false);
 
   const handlePlay = () => {
-    videoRef.current.play();
-    setPlaying(true);
+    handleToggleOpen();
+    setTimeout(() => {
+      videoRef.current.play();
+    }, 100);
   };
 
+  const handleToggleOpen = () => setOpened(!opened);
+
   return (
-    <div className="flex flex-col-reverse lg:flex-row gap-5 lg:gap-[60px]">
-      <div className="w-full max-w-full md:max-w-[470px]">
-        <p className="w-full max-w-[333px] font-bold text-[35px] lg:text-[45px] leading-[45px] lg:leading-[50px] mb-5 lg:mb-6">
-          Here Goes a Big Title About Our Medbed
-        </p>
-        <p className="font-light text-[15px] lg:text-base mb-5 lg:mb-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed cididunt
-          ut labore et dolore magna aliquaenim ad miniaqu is nostruex ercitation
-          ullamco laboris nisi ut aliquip ex ea co cididunt ut labore et dolore
-          magna aliquaenim ad miniaqu is nisi ut aliq cididunt ut labore et
-          dolore magna
-        </p>
-        <div className="grid gap-2.5 mb-7 lg:mb-8">
-          <div className="flex items-center gap-3.5">
-            <span className="w-1.5 h-1.5 rounded-full bg-[#FDD61A]" />
-            <span className="font-light text-base">Disease Type and Name</span>
+    <>
+      <div className="flex flex-col-reverse lg:flex-row gap-5 lg:gap-[60px]">
+        <div className="w-full max-w-full md:max-w-[470px]">
+          <p className="w-full max-w-[333px] font-bold text-[35px] lg:text-[45px] leading-[45px] lg:leading-[50px] mb-5 lg:mb-6">
+            Here Goes a Big Title About Our Medbed
+          </p>
+          <p className="font-light text-[15px] lg:text-base mb-5 lg:mb-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            cididunt ut labore et dolore magna aliquaenim ad miniaqu is nostruex
+            ercitation ullamco laboris nisi ut aliquip ex ea co cididunt ut
+            labore et dolore magna aliquaenim ad miniaqu is nisi ut aliq
+            cididunt ut labore et dolore magna
+          </p>
+          <div className="grid gap-2.5 mb-7 lg:mb-8">
+            <div className="flex items-center gap-3.5">
+              <span className="w-1.5 h-1.5 rounded-full bg-[#FDD61A]" />
+              <span className="font-light text-base">
+                Disease Type and Name
+              </span>
+            </div>
+            <div className="flex items-center gap-3.5">
+              <span className="w-1.5 h-1.5 rounded-full bg-[#FDD61A]" />
+              <span className="font-light text-base">
+                Disease Type and Name
+              </span>
+            </div>
+            <div className="flex items-center gap-3.5">
+              <span className="w-1.5 h-1.5 rounded-full bg-[#FDD61A]" />
+              <span className="font-light text-base">
+                Disease Type and Name
+              </span>
+            </div>
+            <div className="flex items-center gap-3.5">
+              <span className="w-1.5 h-1.5 rounded-full bg-[#FDD61A]" />
+              <span className="font-light text-base">
+                Disease Type and Name
+              </span>
+            </div>
+            <div className="flex items-center gap-3.5">
+              <span className="w-1.5 h-1.5 rounded-full bg-[#FDD61A]" />
+              <span className="font-light text-base">
+                Disease Type and Name
+              </span>
+            </div>
+            <div className="flex items-center gap-3.5">
+              <span className="w-1.5 h-1.5 rounded-full bg-[#FDD61A]" />
+              <span className="font-light text-base">
+                Disease Type and Name
+              </span>
+            </div>
           </div>
-          <div className="flex items-center gap-3.5">
-            <span className="w-1.5 h-1.5 rounded-full bg-[#FDD61A]" />
-            <span className="font-light text-base">Disease Type and Name</span>
-          </div>
-          <div className="flex items-center gap-3.5">
-            <span className="w-1.5 h-1.5 rounded-full bg-[#FDD61A]" />
-            <span className="font-light text-base">Disease Type and Name</span>
-          </div>
-          <div className="flex items-center gap-3.5">
-            <span className="w-1.5 h-1.5 rounded-full bg-[#FDD61A]" />
-            <span className="font-light text-base">Disease Type and Name</span>
-          </div>
-          <div className="flex items-center gap-3.5">
-            <span className="w-1.5 h-1.5 rounded-full bg-[#FDD61A]" />
-            <span className="font-light text-base">Disease Type and Name</span>
-          </div>
-          <div className="flex items-center gap-3.5">
-            <span className="w-1.5 h-1.5 rounded-full bg-[#FDD61A]" />
-            <span className="font-light text-base">Disease Type and Name</span>
-          </div>
+          <button className="w-full lg:w-auto h-11 px-11 rounded-[11px] font-medium text-sm bg-gradient-to-br from-[#F7DEE8] via-[#E7E6FC] via-30% via-[#E4F1FE] via-70% to-[#E8EEF3] hover:opacity-50 transition">
+            Request a Quotation
+          </button>
         </div>
-        <button className="w-full lg:w-auto h-11 px-11 rounded-[11px] font-medium text-sm bg-gradient-to-br from-[#F7DEE8] via-[#E7E6FC] via-30% via-[#E4F1FE] via-70% to-[#E8EEF3] hover:opacity-50 transition">
-          Request a Quotation
-        </button>
-      </div>
-      <div className="w-full md:max-w-[494px] max-w-full h-full object-cover rounded-[27px] overflow-hidden relative">
-        <video
-          className="w-full h-full object-cover"
-          play={true}
-          ref={videoRef}
-          onEnded={() => setPlaying(false)}
-        >
-          <source src={Video} type="video/mp4" />
-        </video>
-        {!playing && (
+        <div className="w-full md:max-w-[494px] max-w-full h-full object-cover rounded-[27px] overflow-hidden relative">
+          <video className="w-full h-full object-cover">
+            <source src={Video} type="video/mp4" />
+          </video>
           <button
             className="w-[86px] h-[86px] flex items-center justify-center rounded-full border border-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white/10 backdrop-blur-[5px] backdrop-brightness-[1.15] transition hover:opacity-80"
             onClick={handlePlay}
@@ -72,9 +83,24 @@ const Hero = () => {
               <img src={PlayIcon} />
             </div>
           </button>
-        )}
+        </div>
       </div>
-    </div>
+      {opened && (
+        <div className="fixed top-0 left-0 w-full h-screen flex items-center justify-center z-50">
+          <div
+            className="absolute top-0 left-0 w-full h-full bg-black opacity-40"
+            onClick={handleToggleOpen}
+          />
+          <video
+            className="w-full max-w-5xl object-cover relative z-10"
+            play={true}
+            ref={videoRef}
+          >
+            <source src={Video} type="video/mp4" />
+          </video>
+        </div>
+      )}
+    </>
   );
 };
 
